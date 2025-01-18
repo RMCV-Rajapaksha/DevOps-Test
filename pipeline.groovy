b151d12d153f309b15b77f852ddea7d2e8818059
RMCV-Rajapaksha <cam.chamara@gmail.com>
2025-01-18 15:26:33 +0530

    Refactor HelloWorld printing logic into a class with multiple print methods

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,3 +1,30 @@
-5.times {
-    println 'Hello, World!'
+class HelloWorldPrinter {
+    String message
+
+    HelloWorldPrinter(String message) {
+        this.message = message
+    }
+
+    void printHelloWorld(int times) {
+        times.times {
+            println message
+        }
+    }
+
+    void printWithExclamation(int times) {
+        times.times {
+            println message + "!"
+        }
+    }
+
+    void printWithNumbering(int times) {
+        (1..times).each { number ->
+            println "${number}: ${message}"
+        }
+    }
 }
+
+def printer = new HelloWorldPrinter('Hello, World')
+printer.printHelloWorld(3)
+printer.printWithExclamation(2)
+printer.printWithNumbering(5)
